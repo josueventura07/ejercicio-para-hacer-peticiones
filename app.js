66d280b3d4f7ea6face7cdd3e26d7659c4583d84
josueventura07 <josueandresventura@gmail.com>
2022-11-15 22:37:20 -0400

    actualizacion1

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require('express')
+
+const port = 9007
+
+const app = express()
+
+app.get('/profile', (peticion, respuesta) => {
+    respuesta.json ({
+        message: 'hola me estoy ejecutando desde la peticion get',
+        name: 'josue',
+        age: '36',
+        country: 'Dominican Republic'
+    })
+})
+
+app.post('/empresas', (peticion, respuesta) => {
+    respuesta.json ([
+        "Globan"
+    ])
+})
+
+app.patch('/hobbies', (peticion, respuesta) => {
+    respuesta.json ([
+        "Baseball", "Baskeckball"
+    ])
+})
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`)
+})
